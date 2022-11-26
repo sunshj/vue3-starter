5017ba9f7c9744b532cc0ff930a0067905a9fe6f
sunshj <sunshj317@qq.com>
2022-11-26 22:07:59 +0800

    build(deps): 修改项目提交规范配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -39,48 +39,21 @@ module.exports = {
     }
   }),
 
-  // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
-  // allowCustomScopes: true,
-
-  // allowTicketNumber: false,
-  // isTicketNumberRequired: false,
-  // ticketNumberPrefix: 'TICKET-',
-  // ticketNumberRegExp: '\\d{1,5}',
-
-  // 针对每一个 type 去定义对应的 scopes，例如 fix
-  /*
-    scopeOverrides: {
-      fix: [
-        { name: 'merge' },
-        { name: 'style' },
-        { name: 'e2eTest' },
-        { name: 'unitTest' }
-      ]
-    },
-    */
-
   // 交互提示信息
   messages: {
     type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
     // 选择 scope: custom 时会出下面的提示
     customScope: '请输入自定义的 scope：',
-    subject: '填写简短精炼的变更描述：\n',
-    body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
-    breaking: '列举非兼容性重大的变更（可选）：\n',
+    subject: '请简要描述提交(必填)：\n',
+    body: '填写详细的变更描述（可选）。使用 "|" 换行：\n',
     footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
     confirmCommit: '确认提交？'
   },
-
-  // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
-  allowBreakingChanges: ['feat', 'fix'],
 
   // 跳过要询问的步骤
   skipQuestions: ['body', 'footer'],
 
   // subject 限制长度
-  subjectLimit: 100,
-  breaklineChar: '|' // 支持 body 和 footer
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true,
+  subjectLimit: 100
 }
