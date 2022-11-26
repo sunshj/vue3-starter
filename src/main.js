501418072f089edbb2d5e30f2a0e10f73477b2d3
sunshj <sunshj317@qq.com>
2022-11-26 21:54:58 +0800

    feat(styles                         (样式相关)): 添加全局样式、logo以及iconfont图标库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-
+import './assets/base.css'
 import App from './App.vue'
 import router from './router'
 
